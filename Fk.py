fe24146cbda5c665d4d870da1ad3768163adbcb5
Fkkhan780 <137475134+Fkkhan780@users.noreply.github.com>
2023-07-10 14:25:48 +0500

    Update Fk.py

--- Fk.py
+++ Fk.py
@@ -1144,7 +1144,7 @@ def rehan():
 	elif gh =='3':
 		chos()
 	elif gh =='4':
-		os.system('https://wa.me/+923062422672?text='+tks')
+		os.system('')
 	elif gh =='0':
 		print('[•] THANKS FOR USE ')
 		time.sleep(3)
